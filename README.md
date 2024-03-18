3a9cbc6dcdbf4db3d3c88f67a8fece41536b113d
Ubiratan S. Freitas <usfreitas@gmail.com>
2024-03-18 14:11:10 +0100

    Include changes after pull request in Blender

--- README.md
+++ README.md
@@ -7,31 +7,30 @@ Preferences -> Add-ons -> Install and pick the downloaded file. Then enable
 the add-on.
 
 ## Use
-This will only work on watertight meshes. On the 3D Viewport in object mode, 
-select the target object. The UI is in the Sidebar under Voxel hollow.
+On the 3D Viewport in object mode, select the target object. The UI is in the
+Sidebar under Voxel hollow.
 
 ### Parameters
 
+#### Offset Direction
+Where the offset surface is created relative to the object.
+ - **INSIDE**: Offset surface inside of object. Use it to hollow out the object.
+ - **OUTSIDE**: Offset surface outside of object. This can  be use to create 
+   a mold of the target object.
+
 #### Offset
-Offset of the generated surface in relation to original mesh. For negative 
-offset values, the generated surface is inside the original mesh, which is
-necessary for hollowing out. 
+Offset of the generated surface in relation to original mesh. 
 
-Positive offset values generate a surface outside the original. This can 
-be use to create a mold of the target object with a wall thickness given by
-the offset.
+#### Voxel Size
+The size of the voxel in the VDB grid. Lower values preserve finer details
+in the offset surface. Too big a value may lead to the inner wall intersecting
+the outer wall. 
 
-#### Resolution
-The resolution of the VDB grid. Too big a value may lead to the inner wall
-intersecting the outer wall. 
-
-#### Join
-If checked, the generated surface will be joined with the targed object, 
-effectively hollowing it. The code will flip the normals of the generated
-surface or the target depending on the sign of the offset.
+#### Hollow Duplicate
+Create hollowed out copy of the object. A copy of the original mesh, with
+modifiers and scale applied is joined with the generated offset surface.
 
 ### Tips
- - Apply scale and modifiers before using.
- - Generate the surface without joining at first to check the parameters.
- - With negative offsets the generated surface will not usually be visible,
-   hide the target to inspect it.
+ - For internal offsets, the target mesh needs to be closed, but not 
+   necessarily manifold. External offsets should work with any mesh.
+ - Outside offset surfaces can be an alternative to the solidify modifier.
--- voxel_hollow.py
+++ voxel_hollow.py
@@ -4,9 +4,9 @@
 
 bl_info = {
     "name": "Voxel hollow",
-    "description": "Hollow out a manifold mesh using VDB.",
+    "description": "Hollow out a manifold mesh using OpenVDB.",
     "author": "Ubiratan Freitas",
-    "version": (1, 0, 0),
+    "version": (1, 1, 0),
     "blender": (3, 5, 0),
     "location": "3D Viewport > Sidebar > Hollow",
     "category": "Object",
@@ -18,32 +18,41 @@ import pyopenvdb as vdb
 import numpy as np
 import math
 
-from bpy.props import BoolProperty, FloatProperty, PointerProperty
+from bpy.props import BoolProperty, FloatProperty, PointerProperty, EnumProperty
 from bpy.types import Operator, Panel, PropertyGroup
 
 
 # Properties
 class HollowSettings(PropertyGroup):
 
-    offset : FloatProperty(
-        name = "Offset",
-        description = "Surface offset in relation to original mesh. Negative -> inwards",
-        default = -5.0,
-        subtype = 'DISTANCE',
+    offset_direction: EnumProperty(
+        items=[
+            ('INSIDE', "Inside", "Offset surface inside of object"),
+            ('OUTSIDE', "Outside", "Offset surface outside of object"),
+            ],
+        name="Offset Direction",
+        description="Where the offset surface is created relative to the object",
+        default='INSIDE',
         )
-
-    resolution : FloatProperty(
-        name = "Resolution",
-        description = "Resolution of the VDB voxel grid",
-        default = 5.0,
-        min = 0.01,
-        subtype = 'DISTANCE',
+    offset: FloatProperty(
+        name="Offset",
+        description="Surface offset in relation to original mesh",
+        default=1.0,
+        subtype='DISTANCE',
+        min=0.0,
+        step=1,
         )
-
-    join : BoolProperty(
-        name = "Join",
-        description = "Join the generated offset mesh to the original object, effectively hollowing it",
-        default = False
+    voxel_size: FloatProperty(
+        name="Voxel size",
+        description="Size of the voxel used for volume evaluation. Lower values preserve finer details",
+        default=1.0,
+        min=0.0001,
+        step=1,
+        subtype='DISTANCE',
+        )
+    make_hollow_duplicate: BoolProperty(
+        name="Hollow Duplicate",
+        description="Create hollowed out copy of the object",
         )
 
 # Test functions for polling
@@ -58,7 +67,7 @@ class OBJECT_PT_hollow(Panel):
     bl_space_type = "VIEW_3D"
     #bl_context = "objectmode"
     bl_region_type = "UI"
-    bl_label = "Hollow"
+    bl_label = "Voxel Hollow"
     bl_category = "Voxel hollow"
     @classmethod
     def poll(cls, context):
@@ -67,86 +76,159 @@ class OBJECT_PT_hollow(Panel):
     def draw(self, context):
         hollow_settings = context.scene.hollow
         layout = self.layout
-        layout.label(text="Voxel Hollow")
-        row = layout.row(align=True)
-        row.prop(hollow_settings, "offset")
-        row = layout.row(align=True)
-        row.prop(hollow_settings, "resolution")
-        row = layout.row(align=True)
-        row.prop(hollow_settings, "join")
-        layout.operator("hollow.create")
+        layout.use_property_split = True
+        layout.use_property_decorate = False
 
-class Hollow_OT_create(Operator):
-    bl_idname = "hollow.create"
-    bl_label = "Create offset surface"
+        layout.prop(hollow_settings, "offset_direction", expand=True)
+        layout.prop(hollow_settings, "offset")
+        layout.prop(hollow_settings, "voxel_size")
+        layout.prop(hollow_settings, "make_hollow_duplicate")
+        layout.operator("mesh.voxel_hollow")
+
+class MESH_OT_voxel_hollow(Operator):
+    bl_idname = "mesh.voxel_hollow"
+    bl_label = "Voxel Hollow"
     bl_description = "Create offset surface"
     bl_options = {'REGISTER', 'UNDO'}
+
+    offset_direction: EnumProperty(
+        items=[
+            ('INSIDE', "Inside", "Offset surface inside of object"),
+            ('OUTSIDE', "Outside", "Offset surface outside of object"),
+            ],
+        name="Offset Direction",
+        description="Where the offset surface is created relative to the object",
+        default='INSIDE',
+        )
+    offset: FloatProperty(
+        name="Offset",
+        description="Surface offset in relation to original mesh",
+        default=1.0,
+        subtype='DISTANCE',
+        min=0.0,
+        step=1,
+        )
+    voxel_size: FloatProperty(
+        name="Voxel size",
+        description="Size of the voxel used for volume evaluation. Lower values preserve finer details",
+        default=1.0,
+        min=0.0001,
+        step=1,
+        subtype='DISTANCE',
+        )
+    make_hollow_duplicate: BoolProperty(
+        name="Hollow Duplicate",
+        description="Create hollowed out copy of the object",
+        )
 
     @classmethod
     def poll(cls, context):
         return is_mode_object(context) and is_active_object_mesh(context)
 
-    def execute(self, context):
-        settings = context.scene.hollow
-        offset = settings.offset
-        resolution = settings.resolution
-        join = settings.join
+    def draw(self, context):
+        layout = self.layout
+        layout.use_property_split = True
+        layout.use_property_decorate = False
 
-        # Target object
+        layout.separator()
+
+        layout.prop(self, "offset_direction", expand=True)
+        layout.prop(self, "offset")
+        layout.prop(self, "voxel_size")
+        layout.prop(self, "make_hollow_duplicate")
+
+    def execute(self, context):
+
+        if not self.offset:
+            return {'FINISHED'}
+
+        # Get target mesh with modifiers
         obj = context.active_object
-        m = obj.data # mesh
+        depsgraph = context.evaluated_depsgraph_get()
+        mesh_target = bpy.data.meshes.new_from_object(obj.evaluated_get(depsgraph))
+
+        # Apply scale, but avoid translating the mesh
+        mat = obj.matrix_world.copy()
+        mat.translation = 0, 0, 0
+        mesh_target.transform(mat)
 
         # Read mesh to numpy arrays
-        nverts = len(m.vertices)
-        ntris = len(m.loop_triangles)
-        verts = np.zeros(3*nverts, dtype=np.float32)
-        tris = np.zeros(3*ntris, dtype=np.int32)
-        m.vertices.foreach_get('co', verts)
+        nverts = len(mesh_target.vertices)
+        ntris = len(mesh_target.loop_triangles)
+        verts = np.zeros(3 * nverts, dtype=np.float32)
+        tris = np.zeros(3 * ntris, dtype=np.int32)
+        mesh_target.vertices.foreach_get("co", verts)
         verts.shape = (-1, 3)
-        m.loop_triangles.foreach_get('vertices', tris)
+        mesh_target.loop_triangles.foreach_get("vertices", tris)
         tris.shape = (-1, 3)
 
         # Generate VDB levelset
-        half_width = max(3.0, math.ceil(abs(offset)/resolution) + 2.0) # half_width has to envelop offset
+        half_width = max(3.0, math.ceil(abs(self.offset) / self.voxel_size) + 2.0) # half_width has to envelop offset
         trans = vdb.Transform()
-        trans.scale(resolution)
+        trans.scale(self.voxel_size)
         levelset = vdb.FloatGrid.createLevelSetFromPolygons(verts, triangles=tris, transform=trans, halfWidth=half_width)
-        
+
         # Generate offset surface
-        newverts, newquads = levelset.convertToQuads(offset)
-        polys = [x for x in newquads]
+        if self.offset_direction == 'INSIDE':
+            newverts, newquads = levelset.convertToQuads(-self.offset)
+            if newquads.size == 0:
+                self.report({'ERROR'}, "Make sure target mesh has closed surface and offset value is less than half of target thickness")
+                return {'FINISHED'}
+        else:
+            newverts, newquads = levelset.convertToQuads(self.offset)
+
+        polys = list(newquads)
 
         # Instantiate new object in Blender
-        mesh = bpy.data.meshes.new(m.name + ' offset')
-        mesh.from_pydata(newverts, [], polys)
-        newobj = bpy.data.objects.new(obj.name + ' offset', mesh)
-        newobj.matrix_world = obj.matrix_world.copy()
-        bpy.context.collection.objects.link(newobj)
+        bpy.ops.object.mode_set(mode='OBJECT')
+        bpy.ops.object.select_all(action='DESELECT')
+        mesh_offset = bpy.data.meshes.new(mesh_target.name + " offset")
+        mesh_offset.from_pydata(newverts, [], polys)
 
-        if not join:
-            # for some reason OpenVDB has inverted normals
-            mesh.flip_normals()
-        else:
-            if offset < 0.0:
-                # offset surface already has normals as they should, see above
-                pass
+        # For some reason OpenVDB has inverted normals
+        mesh_offset.flip_normals()
+        obj_offset = bpy.data.objects.new(obj.name + " offset", mesh_offset)
+        obj_offset.matrix_world.translation = obj.matrix_world.translation
+        bpy.context.collection.objects.link(obj_offset)
+        obj_offset.select_set(True)
+        context.view_layer.objects.active = obj_offset
+
+        if self.make_hollow_duplicate:
+            obj_hollow = bpy.data.objects.new(obj.name + " hollow", mesh_target)
+            bpy.context.collection.objects.link(obj_hollow)
+            obj_hollow.matrix_world.translation = obj.matrix_world.translation
+            obj_hollow.select_set(True)
+            if self.offset_direction == 'INSIDE':
+                mesh_offset.flip_normals()
             else:
-                # offset surface is outside, correct normals, see above
-                mesh.flip_normals()
-                # original surface is inside, flip its normals
-                m.flip_normals()
-            bpy.ops.object.select_all(action='DESELECT')
-            newobj.select_set(True)
-            obj.select_set(True)
-            context.view_layer.objects.active = obj
+                mesh_target.flip_normals()
+            context.view_layer.objects.active = obj_hollow
             bpy.ops.object.join()
+        else:
+            bpy.data.meshes.remove(mesh_target)
+
+        hollow_settings = context.scene.hollow
+        hollow_settings.offset_direction = self.offset_direction
+        hollow_settings.offset = self.offset
+        hollow_settings.voxel_size = self.voxel_size
+        hollow_settings.make_hollow_duplicate = self.make_hollow_duplicate
 
         return {'FINISHED'}
+
+    def invoke(self, context, event):
+        if context.mode == 'EDIT_MESH':
+            bpy.ops.object.mode_set(mode='OBJECT')
+        hollow_settings = context.scene.hollow
+        self.offset_direction = hollow_settings.offset_direction
+        self.offset = hollow_settings.offset
+        self.voxel_size = hollow_settings.voxel_size
+        self.make_hollow_duplicate = hollow_settings.make_hollow_duplicate
+        return self.execute(context)
 
 classes = (
         HollowSettings,
         OBJECT_PT_hollow,
-        Hollow_OT_create,
+        MESH_OT_voxel_hollow,
         )
 # Register
 def register():
